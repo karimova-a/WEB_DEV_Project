2728ce5013ea5be99cef1fe28634a277b860a705
kamshat188 <kamshat.b18@mail.ru>
2026-04-18 15:30:12 +0500

    пока что добавила модели классов  и регистрацию админки

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
-# Register your models here.
+from .models import Genre, Movie, WatchlistItem, MoodEntry, UserProfile
+admin.site.register(Genre)
+admin.site.register(Movie)
+admin.site.register(WatchlistItem)
+admin.site.register(MoodEntry)
+admin.site.register(UserProfile)
