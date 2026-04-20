7f60429dc1ab8073b130234cae38130d82f09674
karimova-a <a_karimova@kbtu.kz>
2026-04-21 02:47:48 +0500

    interceptors final

--- frontend/src/app/interceptors/jwt.interceptor.ts
+++ frontend/src/app/interceptors/jwt.interceptor.ts
@@ -1,3 +1,12 @@
 import {HttpEvent, HttpInterceptorFn, HttpRequest} from '@angular/common/http';
-import {Observable} from 'rxjs';
+
+export const jwtInterceptor: HttpInterceptorFn = (req, next) => {
+  const token = localStorage.getItem('access_token');
+  if (token) {
+    req = req.clone({
+      setHeaders: { Authorization: `Bearer ${token}` }
+    });
+  }
+  return next(req);
+};
 
